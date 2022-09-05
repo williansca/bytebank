1a4d295f8a20f9d204a9229e5adff351888058ec
Willian Scappini Britez <74613589+williansca@users.noreply.github.com>
2022-09-05 00:58:26 -0300

    bytebank project 1.0

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -1,15 +1,38 @@
 package bytebank;
 
+//composicao de objetos
 public class Conta {
-	
+
 	double saldo;
-	
+
 	int agencia;
-	
+
 	int numero;
-	
+
 	String titular;
+
+	public void deposita(double valor) {
+		// this to the acc that invoked the method
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo > 0) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
 	
-	
+	public boolean transfere(double valor, Conta destino) {
+		
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			//valor += destino.saldo;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 
 }
---
+++ src/bytebank/TestaMetodo.java
@@ -0,0 +1,29 @@
+package bytebank;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+
+		// contaDoPaulo.saca(50);
+		boolean conseguiuSacar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuSacar);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+
+		if (contaDaMarcela.transfere(100, contaDoPaulo)) {
+			System.out.println("Transferência feita com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+	}
+
+}
